bcf93ff47e634d8ef712f6451b80587929ef862a
Alejandro Rivera <alejandro@redmart.com>
2016-11-12 18:08:17 +0800

    Reformatted code to adhere to new Checkstyle rules.
    No functionality changes introduced.

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,8 +1,7 @@
 package controllers;
 
 import play.mvc.*;
-
-import views.html.*;
+import views.html.index;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -10,14 +9,14 @@ import views.html.*;
  */
 public class HomeController extends Controller {
 
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
-    public Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+  /**
+   * An action that renders an HTML page with a welcome message.
+   * The configuration in the <code>routes</code> file means that
+   * this method will be called when the application receives a
+   * <code>GET</code> request with a path of <code>/</code>.
+   */
+  public Result index() {
+    return ok(index.render("Your new application is ready."));
+  }
 
 }
